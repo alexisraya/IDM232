84be93eff5f34b820c09c1d273dc63b6a05132b4
alexisraya <aar335@drexel.edu>
2022-11-13 13:42:33 -0500

    :lipstick: stylize form

--- public_html/final/create/index.php
+++ public_html/final/create/index.php
@@ -24,29 +24,29 @@ $result = mysqli_query($db_connection, $query);
       <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
           <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-            <form action="<?php echo site_url(); ?>/_includes/process-create-users.php" method="POST">
+            <form class="form" action="<?php echo site_url(); ?>/_includes/process-create-users.php" method="POST">
               <div class="block">
-                <label for="">Recipe Title</label>
+                <label class="form-label" for="">Recipe Title:</label>
                 <input class="border-black border-2" type="text" name="recipe_title">
               </div>
 
               <div class="block">
-                <label for="">Cook Time</label>
-                <input class="border-black border-2" type="number" name="cook_time">
+                <label class="form-label" for="">Cook Time:</label>
+                <input class="border-black border-2 cook-time" type="number" name="cook_time">
               </div>
 
 
               <div class="block">
-                <label for="">Ingredients</label>
+                <label class="form-label" for="">Ingredients:</label>
                 <input class="border-black border-2 js-tinymce" type="text" name="ingredients">
               </div>
               <div class="block">
-                <label for="">Directions</label>
+                <label class="form-label" for="">Directions:</label>
                 <input class="border-black border-2 js-tinymce" type="text" name="directions">
               </div>
               <input
-                class="nline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                type="submit" value="submit">
+                class="nline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto submit-button"
+                type="submit" value="Add Recipe">
             </form>
           </div>
         </div>
